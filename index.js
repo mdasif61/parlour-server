e186b1468e44c8eaeff968ca7746305898e8e62c
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-05 00:34:58 +0600

    unique service api create

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const express=require('express')
-const app=express();
-const port=process.env.PORT || 5000;
-const cors=require('cors');
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const jwt = require('jsonwebtoken')
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Woman Parlour Running')
-})
+app.get("/", (req, res) => {
+  res.send("Woman Parlour Running");
+});
 // parlour
 // qX82SUYDvg6zmAtI
 
+const verifyJWT=(req,res,next)=>{
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.send({error:true, message:'unauthorized'})
+  }
+  const token=authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error,decoded)=>{
+    if(error){
+      return res.send({error:true, message:'unauthorized'})
+    }
+    req.decoded=decoded;
+    next()
+  })
+}
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://parlour:qX82SUYDvg6zmAtI@cluster0.kuomool.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://parlour:qX82SUYDvg6zmAtI@cluster0.kuomool.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,31 +39,58 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     await client.connect();
 
-    const serviceCollection=client.db('woman_parlour').collection('services');
+    const serviceCollection = client.db("woman_parlour").collection("services");
+    const userCollection = client.db("woman_parlour").collection("users");
 
-    // service api
-    app.get('/services',async(req,res)=>{
-        const limit=parseInt(req.query?.limit) || 3
-        const result=await serviceCollection.find().limit(limit).toArray()
-        res.send(result)
+    app.post('/jwt',(req,res)=>{
+      const loggedUser=req.body;
+      const token=jwt.sign(loggedUser,process.env.ACCESS_TOKEN, {expiresIn:'1h'});
+      res.send({token})
     })
 
     // total services count
-    app.get('/total-services',async(req,res)=>{
-        const result=await serviceCollection.estimatedDocumentCount();
-        res.send({total:result})
+    app.get("/total-services", async (req, res) => {
+      const result = await serviceCollection.estimatedDocumentCount();
+      res.send({ total: result });
+    });
+
+    // service api
+    app.get("/services", async (req, res) => {
+      const limit = parseInt(req.query?.limit) || 3;
+      const result = await serviceCollection.find().limit(limit).toArray();
+      res.send(result);
+    });
+
+    // unique bookings api
+    app.get('/bookings/:id',verifyJWT,async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await serviceCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      const result=await userCollection.insertOne(user)
+      res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,6 +98,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log('women parlour server running port : ',port)
-})
+app.listen(port, () => {
+  console.log("women parlour server running port : ", port);
+});
