941efbc504a7af636d226c9ee8d99059f18192d6
ikbal hossain asif <asifikbalgfh312@gamil.com>
2023-06-04 19:30:03 +0600

    limit api

--- index.js
+++ index.js
@@ -10,6 +10,50 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Woman Parlour Running')
 })
+// parlour
+// qX82SUYDvg6zmAtI
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://parlour:qX82SUYDvg6zmAtI@cluster0.kuomool.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const serviceCollection=client.db('woman_parlour').collection('services');
+
+    // service api
+    app.get('/services',async(req,res)=>{
+        const limit=parseInt(req.query?.limit) || 3
+        const result=await serviceCollection.find().limit(limit).toArray()
+        res.send(result)
+    })
+
+    // total services count
+    app.get('/total-services',async(req,res)=>{
+        const result=await serviceCollection.estimatedDocumentCount();
+        res.send({total:result})
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port,()=>{
     console.log('women parlour server running port : ',port)
