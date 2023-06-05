7e0386ac98da47539e2307e180c59afec572e408
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-05 23:22:47 +0600

    payment method

--- index.js
+++ index.js
@@ -105,6 +105,13 @@ async function run() {
       const result=await bookedCollection.find(query).toArray()
       res.send(result)
     })
+    
+    app.delete('/booked/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await bookedCollection.deleteOne(query);
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
