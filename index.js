89504ae4d0e1b80d77a34ca64da0f236b3eac393
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-05 22:00:34 +0600

    delete api complete

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
       res.send(result)
     })
 
+    // delete service
+    app.delete('/booked/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await bookedCollection.deleteOne(query)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
